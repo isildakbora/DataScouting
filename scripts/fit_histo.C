02d0603740e0920b89e5b46dbde8105003d4ec70
isildakbora <bora.isildak@cern.ch>
2014-02-17 19:13:58 +0200

    Update fit_histo.C

--- scripts/fit_histo.C
+++ scripts/fit_histo.C
@@ -16,10 +16,12 @@ using namespace RooFit ;
 
 void fit_histo()
 {
+    ROOT::Math::MinimizerOptions::SetDefaultMinimizer("Minuit2");
     //gROOT->ProcessLine(".L DSComp.C");
     TString histoname;
     TH1F *hDeltapT[8][5];
-    TFile *f = new TFile("Delta_pT_histograms.root");
+    TFile *f = new TFile("Delta_pT_histograms_legends.root");
+    TFile *fresolution = new TFile("Delta_pT_Resolution.root", "RECREATE");
     TCanvas *i_can[8][5];
     TString name;
     float pTbins[]  = {30., 103., 206., 309., 413., 515., 618., 721., 1237.};
@@ -35,10 +37,9 @@ void fit_histo()
             histoname      = "Delta_pT_"+TString::Format("%.0f",pTbins[i])+"_"+TString::Format("%.0f",pTbins[i+1])+"_eta_"+TString::Format("%2.1f",j*0.5)+"_"+TString::Format("%2.1f",(j+1)*0.5);
             hDeltapT[i][j] = (TH1F*)f->FindObjectAny(histoname);
             i_can[i][j] =  new TCanvas(histoname);
-            if(hDeltapT[i][j]->GetEntries()>3000)hDeltapT[i][j]->Rebin(2);
-            else hDeltapT[i][j]->Rebin(1);
-        }
-        
+            if(hDeltapT[i][j]->GetEntries()>1000)hDeltapT[i][j]->Rebin(4);
+            else hDeltapT[i][j]->Rebin(2);
+        }   
     }
 
     TCanvas *can = new TCanvas("DSComparison","DSComparison",1200,1200);
@@ -54,29 +55,30 @@ void fit_histo()
             std::cout << i << "\t" << j << std::endl;
             name = "Delta_pT_"+TString::Format("%.0f",pTbins[i])+"_"+TString::Format("%.0f",pTbins[i+1])+"_eta_"+TString::Format("%2.1f",j*0.5)+"_"+TString::Format("%2.1f",(j+1)*0.5);
            
-            gStyle->SetOptStat(1101);
+            gStyle->SetOptStat("nemr");
+            gStyle->SetOptFit(0011);
 
             maxVal = hDeltapT[i][j]->GetBinContent(hDeltapT[i][j]->GetMaximumBin());
             mean   = hDeltapT[i][j]->GetMean();
             RMS    = hDeltapT[i][j]->GetRMS();
 
-            cbfit[i][j] = new TF1(name, DoubleCrystalBallFunction, mean-4*RMS, mean+4*RMS, 7);
+            cbfit[i][j] = new TF1(name, DoubleCrystalBallFunction, mean-2*RMS, mean+2*RMS, 7);
             cbfit[i][j]->SetParNames("A_{1}","#alpha_{1}","#alpha_{2}","n_{1}","n_{2}","#mu","#sigma");
-            cbfit[i][j]->SetParameters(maxVal, 2., 2., 2., 2., mean, RMS);
-            cbfit[i][j]->SetParLimits(0, 0.9*maxVal, 1.1*maxVal);
+            cbfit[i][j]->SetParameters(maxVal, 0.9, 3.3, 50., 50., mean, RMS);
+            /*cbfit[i][j]->SetParLimits(0, 0.9*maxVal, 1.1*maxVal);
             cbfit[i][j]->SetParLimits(1, 0., 5.);
             cbfit[i][j]->SetParLimits(2, 0., 5.);
-            cbfit[i][j]->SetParLimits(3, 0., 20);
-            cbfit[i][j]->SetParLimits(4, 0., 20);
-            cbfit[i][j]->SetParLimits(5, 0.9*mean, 1.1*mean);
-            cbfit[i][j]->SetParLimits(6, 0.8*RMS, 1.2*RMS);
+            cbfit[i][j]->SetParLimits(3, 0., 100);
+            cbfit[i][j]->SetParLimits(4, 0., 100);
+            //cbfit[i][j]->SetParLimits(5, 0.5*mean, 1.5*mean);
+            cbfit[i][j]->SetParLimits(6, 0.8*RMS, 1.2*RMS);*/
             cbfit[i][j]->SetLineColor(kRed+1);
 
             if(i*5+j+1<10)can->cd(i*5+j+1);
             //gPad->SetLogy();
-            hDeltapT[i][j]->Fit(name, "REMQ0+");
-            hDeltapT[i][j]->Fit(name, "REMQ");
-            hDeltapT[i][j]->GetXaxis()->SetRangeUser(mean-6*RMS, mean+6*RMS);
+            hDeltapT[i][j]->Fit(name, "REQ0+");
+            hDeltapT[i][j]->Fit(name, "REQ");
+            hDeltapT[i][j]->GetXaxis()->SetRangeUser(mean-2*RMS, mean+2*RMS);
             hDeltapT[i][j]->GetXaxis()->SetTitle("#Deltap_{T}/p_{T}");
             hDeltapT[i][j]->GetXaxis()->SetLabelSize(0.04);
             hDeltapT[i][j]->GetYaxis()->SetLabelSize(0.04);
@@ -85,9 +87,17 @@ void fit_histo()
             hDeltapT[i][j]->Draw("E1");
             cbfit[i][j]   ->Draw("same");
             i_can[i][j]   ->SaveAs(name.ReplaceAll(".", "_")+".pdf");
+            fresolution->cd();
+            cbfit[i][j]->Write();
         }
     }
-    can->SaveAs("DSCompAllInOne.pdf");
+    can->SaveAs("DSCompAllInOne.pdf");   
+}
+
+
+double calculate_Mjj(double )
+{
+
 }
 
 Double_t DoubleCrystalBallFunction(Double_t *xx, Double_t *par) {
