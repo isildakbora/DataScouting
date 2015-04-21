d3d9ebe505ef55d22db412a2ab5ed95b2c358066
Bora Isildak <bora.isildak@cern.ch>
2015-04-21 10:15:35 +0300

    Update run_stat.py

--- test/Limits/run_stat.py
+++ test/Limits/run_stat.py
@@ -2,8 +2,7 @@
 import os, sys
 
 nJobs = 200
-masspoints = []
-masspoints.append(int(sys.argv[2]))
+masspoints = [1000, 2000, 3000, 400]
 for i in masspoints:
 	for j in xrange(nJobs):
 		print './stats_parallel ' + str(sys.argv[1])+ ' ' + str(i)+ ' '+ str(j)  + ' > log_'+str(i)+'_'+ str(sys.argv[1]) + '_' + str(j) + '.txt &'
